d2fa1bad364a8d102ef0cd4ed194c7eb5f710aa6
Artem Ouliankine <artem.ouliankine@bellevuecollege.edu>
2019-08-01 13:51:04 -0700

    resume rendered

--- NodejsResume/NodejsResume/app.tsx
+++ NodejsResume/NodejsResume/app.tsx
@@ -1,1 +1,15 @@
-﻿
+﻿declare var require: any
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+export class Hello extends React.Component {
+    render() {
+        return (
+            <iframe src="https://resume.creddle.io/embed/394d5ay1cex"
+                width="850" height="1100" seamless></iframe>
+        );
+    }
+}
+
+ReactDOM.render(<Hello />, document.getElementById('root'));
